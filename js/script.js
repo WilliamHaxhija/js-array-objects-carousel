ed1d11d125b8c3447d77a305c94ad197c98b2b48
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-21 22:01:29 +0100

    end project

--- js/script.js
+++ js/script.js
@@ -1,40 +1,82 @@
-/*
-Dato un array di oggetti letterali con:
- - url dell’immagine
- - titolo
- - descrizione
-Creare un carosello come nella foto allegata.
-Milestone 0:
-Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
-Milestone 1:
-Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
-Al click dell'utente sulle frecce verso alto o basso, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
-Milestone 2:
-Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
-BONUS:
-Una volta terminate tutte e 3 le milestone, potete contattare i tutor che vi assegneranno dei bonus.
-*/
-
 const images = [
     {
-        image: 'img/01.webp',
+        image: '01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
+        image: '02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
+        image: '03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
+        image: '04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
+        image: '05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+//Seleziono gli elementi del DOM in cui inserire le immagini 
+
+const mainThumb = document.querySelector('.main-thumbnail');
+const smallThumbs = document.querySelectorAll('[class|=small-thumbnail]');
+
+//Inserisco le immagini
+
+for (let i = 0; i < smallThumbs.length; i++) {
+    thisThumb = smallThumbs[i];
+    let img = images[i].image;
+    thisThumb.innerHTML = `<img src="./img/${img}" alt="${images[i].title}">`;
+}
+
+//Creo evento per il click
+
+const arrowDown = document.querySelector('.fa-circle-chevron-down');
+let i = 0;
+const smallImages = document.querySelectorAll('.small-thumbnails-ctn img');
+if (smallImages[i] !== i) {
+    smallImages[i].classList.add('active');
+}
+createElementInDom(images[i].image, images[i].title, images[i].text)
+
+arrowDown.addEventListener('click', function() {
+    smallImages[i].classList.remove('active');
+    i++;
+    if (i >= images.length) {
+        i = 0;
+    }
+    createElementInDom(images[i].image, images[i].title, images[i].text)
+    if (smallImages[i] !== i) {
+        smallImages[i].classList.add('active');
+    }
+});
+
+const arrowUp = document.querySelector('.fa-circle-chevron-up');
+
+arrowUp.addEventListener('click', function() {
+    smallImages[i].classList.remove('active');
+    i--;
+    if (i < 0) {
+        i = images.length - 1;
+    }
+    createElementInDom(images[i].image, images[i].title, images[i].text)
+    if (smallImages[i] !== i) {
+        smallImages[i].classList.add('active');
+    }
+});
+
+//FUNCTIONS
+
+function createElementInDom (image, title, text, number) {
+    mainThumb.innerHTML = `<img src="./img/${image}" alt="">
+                           <div class="img-description">
+                                <h3>${title}<h3>
+                                <p> ${text}<p>
+                           </div>`;
+}
